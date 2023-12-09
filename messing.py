512f2be5798bb558f49bdfe9d52771d8c3e3272b
chocolate-juice <140909474+chocolate-juice@users.noreply.github.com>
2023-12-09 13:56:10 -0500

    Add files via upload

---
+++ messing.py
@@ -0,0 +1,16 @@
+first = input('Enter your First Name: ')
+last = input('Enter your Last Name: ')
+a = 'Hello ' + first + ' ' + last
+print(a)
+print('Welcome to Birth Year Calculator!')
+
+age = int(input('Now enter your age: '))
+birth_year = 2023 - age 
+b = 'You were born in ' + str(birth_year) 
+print(b)
+
+nage = age + 1
+c = 'You will be ' + str(nage) + ' next year!'
+print(c)
+
+
